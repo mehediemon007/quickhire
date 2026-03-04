10deee8f00c36a192d276a57740a04f490e69a5c
mehediemon007 <mehediemon7@gmail.com>
2026-03-04 16:04:12 +0600

    fix(server): resolve CORS error

--- server/server.ts
+++ server/server.ts
@@ -11,9 +11,24 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "http://localhost:3000"
+]
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: (origin, callback) => {
+      
+      if(!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+
+    },
     credentials: true,
   })
 );
