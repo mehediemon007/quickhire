65c45158b9b446c1042ea95ac20ed40ca56d7246
mehediemon007 <mehediemon8=7@gmail.com>
2026-03-05 03:27:59 +0600

    build: config swagger to async with postman

--- server/swagger.ts
+++ server/swagger.ts
@@ -3,10 +3,10 @@ import swaggerAutogen from 'swagger-autogen';
 const doc = {
     info: { title: 'My API', description: 'Auto-generated API Docs' },
     host: 'localhost:5000',
-    schemes: ['http', 'https'],
+    schemes: ['http'],
     basePath: '/api'
 };
 
 const outputFile = './swagger-output.json';
-const endpointsFiles = ['./server.ts', './routes/jobRoutes.ts', './routes/applicationRoutes.ts', './routes/authRoutes.ts'];
+const endpointsFiles = ['./server.ts',];
 swaggerAutogen({ openapi: '3.0.0' })(outputFile, endpointsFiles, doc);
