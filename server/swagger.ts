240fd70b24554c7039613741c484dd8eeb9b01b3
mehediemon007 <mehediemon8=7@gmail.com>
2026-03-05 00:51:53 +0600

    build: configure swagger route scanning

--- server/swagger.ts
+++ server/swagger.ts
@@ -4,8 +4,9 @@ const doc = {
     info: { title: 'My API', description: 'Auto-generated API Docs' },
     host: 'localhost:5000',
     schemes: ['http', 'https'],
+    basePath: '/api'
 };
 
 const outputFile = './swagger-output.json';
-const endpointsFiles = ['./server.ts', './routes/*.ts'];
-swaggerAutogen()(outputFile, endpointsFiles, doc);
+const endpointsFiles = ['./server.ts', './routes/jobRoutes.ts', './routes/applicationRoutes.ts', './routes/authRoutes.ts'];
+swaggerAutogen({ openapi: '3.0.0' })(outputFile, endpointsFiles, doc);
